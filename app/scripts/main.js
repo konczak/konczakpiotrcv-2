b66cab5cfdf82f3f00eda3272554732d2c3f5dd4
Piotr Konczak <konczak.piotrek@gmail.com>
2017-10-12 21:55:43 +0200

    make navbar transparent on top and background elsewhere

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,3 +3,16 @@ function showOtherSkills(caller, otherSkillsId) {
     .classList.remove('d-none');
   caller.classList.add('disabled');
 }
+
+function scroll() {
+  var $window = $(window),
+    navbar = $('nav');
+
+  if ($window.scrollTop() > 0) {
+    navbar.addClass('top-nav-collapse');
+  } else {
+    navbar.removeClass('top-nav-collapse');
+  }
+}
+
+$(window).scroll(scroll);
